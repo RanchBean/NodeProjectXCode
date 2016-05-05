812bc4a45cc635d50e61b88c505684e028f893a3
Ethan throndsen <Ethunderson1@gmail.com>
2016-05-05 12:30:07 -0600

    added get size method to the hash table

--- Model/CTECHashTable.cpp
+++ Model/CTECHashTable.cpp
@@ -5,5 +5,25 @@
 //  Created by Throndsen, Ethan on 5/5/16.
 //  Copyright © 2016 CTEC. All rights reserved.
 //
-
+#include <iostream>
 #include "CTECHashTable.hpp"
+using namespace std;
+
+template <class Type>
+CTECHashTable<Type> :: CTECHashTable()
+{
+    this->size = 0;
+    this->capacity = 101;
+    this->efficiencyPercentage = .667;
+    this->internalStorage = new Type[capacity];
+}
+template <class Type>
+CTECHashTable<Type> :: ~CTECHashTable()
+{
+    delete [] internalStorage;
+}
+template <class Type>
+int CTECHashTable :: getSize()
+{
+    return this->size;
+}
